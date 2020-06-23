86b011501ad3e6ab823dbc8981686a3354b50eee
smardone <shaun.mardones@gmail.com>
2020-06-22 21:43:30 -0400

    refactored functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+function taskFormHandler(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package the date up as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
+    // Creat List Item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // Creat div to hold task info and add to list item
+    taskInfoEl = document.createElement("div");
+    // Give it a class name
+    taskInfoEl.className = "task-info";
+    // Add HTML content to the div
+    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name + "<h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
+}
+formEl.addEventListener("submit", taskFormHandler);
