1b002c94cbbe1f2042a0b1dde96d20537c287f2b
smardone <shaun.mardones@gmail.com>
2020-06-26 21:38:37 -0400

    added user friendly visuals to drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,6 +187,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed");
     }
 }
 
@@ -202,10 +203,19 @@ var dropTaskHandler = function(event) {
     else if (statusType = "tasks-completed") statusSelectEl.selectedIndex = 2;
     
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
